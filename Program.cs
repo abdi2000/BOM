1316ec36081a8bea70234cb9953e32406b606261
Abdeali Mandviwala <amandviwala@da.com.au>
2024-01-04 17:18:33 +1030

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+CalculateAverageTemperature();
+
+static void CalculateAverageTemperature()
+{
+    //Deserialize the json file
+    string fileName = "weatherdata.json";
+    string jsonString = File.ReadAllText(fileName);
+    Root weatherData = JsonSerializer.Deserialize<Root>(jsonString)!;
+
+    //Calculate the average of air temperature
+    double averageaAirTemperature = weatherData.observations.data.Average(x => x.air_temp);
+
+    Console.WriteLine("The Average Air Temperature is : {0:0.00}", averageaAirTemperature);
+
+    Console.Read();
+}
